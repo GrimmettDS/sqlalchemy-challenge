628b96c43e321c7de946c733a5d19976714b8b15
GrimmettDS <davidsgrimmett@gmail.com>
2021-01-25 11:35:18 -0500

    Flask update.

--- Climate_App.py
+++ Climate_App.py
@@ -59,6 +59,7 @@ def precipitation():
 
     return jsonify(prcp_data)
 
+
 @app.route("/api/v1.0/stations")
 def stations():
 
@@ -75,6 +76,78 @@ def stations():
         station_list.append(station_dict)
 
     return jsonify(station_list)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    
+    session = Session(engine)
+    
+    # Find the most recent date in the data set.
+    recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    
+    # Calculate the date one year from the last date in data set.
+    recent_date = recent_date[0]
+    past_year = dt.datetime.strptime(recent_date, "%Y-%m-%d") - dt.timedelta(days=365)  
+    
+    # Query the dates and temperature observations of the most active station for the last year of data.
+
+    most_active_station = session.query(Measurement.station, Measurement.date, Measurement.tobs).filter(Measurement.date >= past_year).order_by(Measurement.date).all()
+    station_list = list(most_active_station)
+    
+    session.close()
+
+    #Return a JSON list of temperature observations (TOBS) for the previous year.
+    return jsonify(station_list)
+
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    
+    session = Session(engine)
+
+    recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    recent_date = recent_date[0]
+    
+    min_temp = session.query(Measurement.tobs, func.min(Measurement.tobs)).filter(Measurement.date.between (start, recent_date))
+    avg_temp = session.query(Measurement.tobs, func.avg(Measurement.tobs)).filter(Measurement.date.between (start, recent_date))
+    max_temp = session.query(Measurement.tobs, func.max(Measurement.tobs)).filter(Measurement.date.between (start, recent_date))
+
+    temp_list = {"Tmin": min_temp[0][0], "Tmax": max_temp[0][0], "Tavg": avg_temp[0][0]}
+
+    start_calc = session.query(Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+    filter(Measurement.date >= start).group_by(Measurement.date).all()
+    
+    calc_list = list(start_calc)
+
+    session.close()
+
+    return jsonify(temp_list, calc_list)
+
+
+@app.route("/api/v1.0/<start>&<end>")
+def start(start, end):
+    
+    session = Session(engine)
+    
+    min_temp = session.query(Measurement.tobs, func.min(Measurement.tobs)).filter(Measurement.date.between (start, end))
+    avg_temp = session.query(Measurement.tobs, func.avg(Measurement.tobs)).filter(Measurement.date.between (start, end))
+    max_temp = session.query(Measurement.tobs, func.max(Measurement.tobs)).filter(Measurement.date.between (start, end))
+
+    temp_list = {"Tmin": min_temp[0][0], "Tmax": max_temp[0][0], "Tavg": avg_temp[0][0]}
+
+    start_calc = session.query(Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+    filter(Measurement.date.between(start, end).group_by(Measurement.date).all()
+    
+    calc_list = list(start_calc)
+
+    session.close()
+
+    return jsonify(temp_list, calc_list)
+    
+
+
+
 
 
 
